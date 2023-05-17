3bc60138fa732a371541f07d41178c345886f849
Gideonkipyegon <gideonkipyegon837@gmail.com>
2023-05-17 13:06:35 +0300

    my first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Main from './Components/Main'
 import Navbar from './Components/Navbar'
-import Footer from './components/Footer'
-import Sidebar from './SideBar'
+import Footer from './Components/Footer'
+import Sidebar from './Components/SideBar'
 
 function App() {
     return (
 
-        <div class = "container">
-        
-        <Navbar />
-        <div class="navbar">
-        <Main />
-        <Sidebar />
-        </div>
-        <Footer />
-        </div >
+        <
+        div class = "container" >
+
+        <
+        Navbar / >
+        <
+        div class = "navbar" >
+        <
+        Main / >
+        <
+        Sidebar / >
+        <
+        /div> <
+        Footer / >
+        <
+        /div >
     );
 }
 
